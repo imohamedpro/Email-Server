e5888d98ada903c469c2f0f887251609ab284f53
Mohamed Magdy <imohamedpro@icloud.com>
2021-12-24 21:47:01 +0200

    First Commit

---
+++ src/main/java/university/project/MailBackend/MailBackendApplication.java
@@ -0,0 +1,13 @@
+package university.project.MailBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/university/project/MailBackend/MailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package university.project.MailBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
