e3ad3f0ad4ffe6f5ea0e821f464eb949f4e89f88
Karim Alaa <karimalaa1912@gmail.com>
2021-12-27 20:01:55 +0200

    added userContact

--- src/main/java/university/project/MailBackend/Model/UserContact.java
+++ src/main/java/university/project/MailBackend/Model/UserContact.java
@@ -1,5 +1,13 @@
 package university.project.MailBackend.Model;
 
+import java.util.HashMap;
+
 public class UserContact {
+    public HashMap<Integer, Contact> contacts;
+    private int nextID;
+    UserContact(){
+        contacts = new HashMap<Integer, Contact>();
+        nextID = 0;
+    }
 
 }
