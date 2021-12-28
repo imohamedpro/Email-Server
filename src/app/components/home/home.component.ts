c8fab1e91200ff578fd1c86879c04e1cecfe92b6
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2021-12-28 17:59:57 +0200

    Added folder manipulation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,17 +7,86 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router, private r: ActivatedRoute) { }
+  customFolders!: string[];
+  isSelected!: boolean[];
+  temp: string = "";
+  doNotEdit: number = -1;
+  constructor(private router: Router, private r: ActivatedRoute) {
+    this.customFolders = [];
+    this.isSelected = [];
+  }
 
   ngOnInit(): void {
   }
-
-  goToCompose(){
-    this.router.navigate(['compose'],{relativeTo: this.r});
+  addNewFolder() {
+    this.customFolders.push(`Folder ${this.customFolders.length + 1}`);
+    console.log(this.customFolders);
+    //for(let i = 0; i < this.customFolders.length -1 ; ++i){
+    this.deSelect(this.customFolders.length - 1);
+    //}
+    this.isSelected[this.customFolders.length - 1] = true;
   }
-  goToFolder(folderNumber: number){
-    let folderName: string = ""; 
-    switch(folderNumber){
+  /*editFolderName(event: any){
+    if(event.key == "Enter"){
+      if(event.target.value == ""){
+        this.customFolders[this.customFolders.length-1] = `Folder ${this.customFolders.length}`;
+      }else{
+        this.customFolders[this.customFolders.length-1] = event.target.value;
+      }
+      this.isNewFolder[this.customFolders.length-1] = false;
+    }
+  }*/
+  deleteFolder(index: number, e: any) {
+    if (index != this.doNotEdit) {
+      e.stopPropagation();
+      this.customFolders.splice(index, 1);
+    }
+  }
+  toggleInput(index: number, e: any) {
+    if (index != this.doNotEdit) {
+      e.stopPropagation();
+      if (this.isSelected[index] == true) {
+        if (this.temp == "" && this.customFolders[index].includes("Folder")) {
+          this.customFolders[index] = `Folder ${index + 1}`;
+        } else if (this.temp == "") {
+          this.customFolders[index] = `Folder ${index + 1}`;
+
+        } else {
+          this.customFolders[index] = this.temp;
+        }
+        this.isSelected[index] = false;
+        this.temp = "";
+      } else {
+        this.deSelect(index);
+        this.customFolders[index] = `Folder ${index + 1}`;
+        this.isSelected[index] = true;
+      }
+    }
+  }
+  deSelect(index: number) {
+    for (let i = 0; i < this.customFolders.length; ++i) {
+      if (i != index) this.isSelected[i] = false;
+    }
+  }
+  deselectAll() {
+    for (let i = 0; i < this.customFolders.length; ++i) {
+      this.isSelected[i] = false;
+    }
+  }
+  avoidP(e: any) {
+    e.stopPropagation();
+  }
+
+  goToCompose() {
+    this.deselectAll();
+    this.doNotEdit = -1;
+    this.router.navigate(['compose'], { relativeTo: this.r });
+  }
+  goToFolder(folderNumber: number) {
+    this.deselectAll();
+    this.doNotEdit = -1;
+    let folderName: string = "";
+    switch (folderNumber) {
       case 1:
         folderName = "inbox";
         break;
@@ -31,9 +100,14 @@ export class HomeComponent implements OnInit {
         folderName = "draft";
         break;
       case 5:
-        folderName="contacts";
+        folderName = "contacts";
         break;
     }
-    this.router.navigate([folderName],{relativeTo: this.r});
+    this.router.navigate([folderName], { relativeTo: this.r });
+  }
+  goToCustomFolder(folderNumber: number) {
+    this.doNotEdit = folderNumber;
+    this.deselectAll();
+    this.router.navigate([this.customFolders[folderNumber].replace(" ", "")], { relativeTo: this.r });
   }
 }
