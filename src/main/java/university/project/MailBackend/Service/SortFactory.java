7e19ddbbdef4a9c11e28b9dd70277a59e4f9c31a
Youssef <youssefmagdi1210@gmail.com>
2021-12-29 15:24:05 +0200

    TestAccountManager

--- src/main/java/university/project/MailBackend/Service/SortFactory.java
+++ src/main/java/university/project/MailBackend/Service/SortFactory.java
@@ -6,15 +6,19 @@ import java.util.Comparator;
 
 public class SortFactory {
     public Comparator<? super EmailHeader> getSortType(String type){
-        return switch (type.toLowerCase()) {
-            // case "subject d" -> Comparator.comparing(EmailHeader::getSubject, String::compareToIgnoreCase).reversed();
-            // case "from d" -> Comparator.comparing(EmailHeader::getFrom, String::compareToIgnoreCase).reversed();
-            // case "priority d" -> Comparator.comparing(EmailHeader::getPriority).reversed();
-            // case "date" -> Comparator.comparing(EmailHeader::getDate);
-            case "subject" -> Comparator.comparing(EmailHeader::getSubject, String::compareToIgnoreCase);
-            case "from" -> Comparator.comparing(EmailHeader::getFrom, String::compareToIgnoreCase);
-            case "priority" -> Comparator.comparing(EmailHeader::getPriority);
-            default -> Comparator.comparing(EmailHeader::getDate);
-        };
+        // case "subject d" -> Comparator.comparing(EmailHeader::getSubject, String::compareToIgnoreCase).reversed();
+        // case "from d" -> Comparator.comparing(EmailHeader::getFrom, String::compareToIgnoreCase).reversed();
+        // case "priority d" -> Comparator.comparing(EmailHeader::getPriority).reversed();
+        // case "date" -> Comparator.comparing(EmailHeader::getDate);
+        switch (type.toLowerCase()) {
+            case "subject":
+                return Comparator.comparing(EmailHeader::getSubject, String::compareToIgnoreCase);
+            case "from":
+                return Comparator.comparing(EmailHeader::getFrom, String::compareToIgnoreCase);
+            case "priority":
+                return Comparator.comparing(EmailHeader::getPriority);
+            default:
+                return Comparator.comparing(EmailHeader::getDate);
+        }
     }
 }
---
+++ src/test/java/TestUnits/Service/TestAccountManager.java
@@ -0,0 +1,49 @@
+package TestUnits.Service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import university.project.MailBackend.Model.Contact;
+import university.project.MailBackend.Model.UserContact;
+import university.project.MailBackend.Model.UserData;
+import university.project.MailBackend.Model.UserInfo;
+import university.project.MailBackend.Service.AccountManager;
+import university.project.MailBackend.Service.StorageAdapter;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestAccountManager {
+    @Mock
+    StorageAdapter storageMock;
+//    TestAccountManager(StorageAdapter storageAdapterMock) {
+//        this.storageMock = storageAdapterMock;
+//    }
+    @Mock
+    UserInfo userInfoMock;
+    @Mock UserData userDataMock;
+    @Mock UserContact userContactMock;
+
+    @Test
+    public void testSignIn( ) {
+        AccountManager accountManager = new AccountManager(storageMock);
+        when(storageMock.getUserInfo(userInfoMock.getEmail())).thenReturn(null);
+        boolean check = accountManager.signIn(userInfoMock);
+//        User info for userInfoMock will be null.
+        assertFalse(check);
+        System.out.println(userInfoMock);
+    }
+    @Test
+    public void testSignUp( ) {
+        AccountManager accountManager = new AccountManager(storageMock);
+        verifyNoInteractions(storageMock);
+//        boolean check = accountManager.signIn(userInfoMock);
+////        User info for userInfoMock will be null.
+//
+//        verify(storageMock).createAcount(userInfoMock,userDataMock,userContactMock);
+//        System.out.println(userInfoMock);
+    }
+}
+
