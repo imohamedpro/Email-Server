8f5d8bfeb048b20cfe443385ff374f61251e4df3
Mohamed Magdy <imohamedpro@icloud.com>
2021-12-31 08:52:05 +0200

    added to backend folder

--- src/main/java/university/project/MailBackend/Model/Filter/Criteria.java
+++
@@ -1,9 +0,0 @@
-package university.project.MailBackend.Model.Filter;
-
-import university.project.MailBackend.Model.Mail;
-
-import java.util.ArrayList;
-
-public interface Criteria {
-    ArrayList<Mail> meetCriteria(ArrayList<Mail> mails);
-}
